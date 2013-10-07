9795a7031f14f9765e4fa47cbaa77dde9fd14403
Alek Zieba <alekrzieba@gmail.com>
2013-10-06 20:15:51 -0400

    ReorganizeProject

--- page_action_script.js
+++
@@ -1,8 +0,0 @@
-// -------------------------------
-// Procrastinate Extension for Chrome: page_action_script.js
-// This is the script for the PAGE ACTION of the extension.
-// Written 8/9/13 by Alek Zieba.
-// --
-// All functions return VOID unless otherwise stated.
-// -------------------------------
-
